6fcdfebc60555cb654aa7f0a84ef6f695d6287e0
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-10-31 10:48:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import tensorflow as tf
-import graphviz
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, LSTM
+import graphviz
 import numpy as np
 import pydot
 
@@ -48,42 +48,36 @@ def create_rnn(num_time_steps):
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
-# Cache the functions to prevent recomputation
-@st.cache_resource
-def get_model(network_type, num_layers, num_neurons, activation_function, kernel_size, pooling_size, num_time_steps):
-    if network_type == "Fully Connected Neural Network (FCNN)":
-        return create_fcnn(num_layers, num_neurons, activation_function)
-    elif network_type == "LeNet":
-        return create_lenet()
-    elif network_type == "Convolutional Neural Network (CNN)":
-        return create_cnn(kernel_size, pooling_size)
-    elif network_type == "Recurrent Neural Network (RNN)":
-        return create_rnn(num_time_steps)
-
 # Streamlit app
 st.title("Neural Network Visualization App")
 
 # User input
 network_type = st.selectbox("Select Network Type", ["Fully Connected Neural Network (FCNN)", "LeNet", "Convolutional Neural Network (CNN)", "Recurrent Neural Network (RNN)"])
 
-# Display relevant input fields based on the selected network type
-if network_type in ["Fully Connected Neural Network (FCNN)", "LeNet"]:
-    num_layers = st.slider("Number of Layers", 1, 10, 3)
-    num_neurons = st.slider("Number of Neurons per Layer", 1, 256, 64)
-    activation_function = st.selectbox("Activation Function", ["relu", "sigmoid", "tanh"])
-    kernel_size = None
-    pooling_size = None
-    num_time_steps = None
-elif network_type == "Convolutional Neural Network (CNN)":
-    kernel_size = st.slider("Kernel Size", 3, 7, 5)
-    pooling_size = st.slider("Pooling Size", 2, 5, 2)
-    num_layers = num_neurons = activation_function = num_time_steps = None
-elif network_type == "Recurrent Neural Network (RNN)":
-    num_time_steps = st.slider("Number of Time Steps", 1, 50, 10)
-    num_layers = num_neurons = activation_function = kernel_size = pooling_size = None
+try:
+    if network_type in ["Fully Connected Neural Network (FCNN)", "LeNet"]:
+        num_layers = st.slider("Number of Layers", 1, 10, 3)
+        num_neurons = st.slider("Number of Neurons per Layer", 1, 256, 64)
+        activation_function = st.selectbox("Activation Function", ["relu", "sigmoid", "tanh"])
+        kernel_size = None
+        pooling_size = None
+        num_time_steps = None
+    elif network_type == "Convolutional Neural Network (CNN)":
+        kernel_size = st.slider("Kernel Size", 3, 7, 5)
+        pooling_size = st.slider("Pooling Size", 2, 5, 2)
+        num_layers = num_neurons = activation_function = num_time_steps = None
+    elif network_type == "Recurrent Neural Network (RNN)":
+        num_time_steps = st.slider("Number of Time Steps", 1, 50, 10)
+        num_layers = num_neurons = activation_function = kernel_size = pooling_size = None
 
-# Create or retrieve the neural network based on the selected type
-model = get_model(network_type, num_layers, num_neurons, activation_function, kernel_size, pooling_size, num_time_steps)
+    # Create or retrieve the neural network based on the selected type
+    if network_type in ["Fully Connected Neural Network (FCNN)", "LeNet", "Convolutional Neural Network (CNN)", "Recurrent Neural Network (RNN)"]:
+        with st.spinner("Building the model..."):
+            model = get_model(network_type, num_layers, num_neurons, activation_function, kernel_size, pooling_size, num_time_steps)
 
-# Display network architecture using Graphviz
-st.graphviz_chart(tf.keras.utils.model_to_dot(model, show_shapes=True, expand_nested=True))
+    # Display network architecture using Graphviz
+    if network_type in ["Fully Connected Neural Network (FCNN)", "LeNet", "Convolutional Neural Network (CNN)", "Recurrent Neural Network (RNN)"]:
+        st.graphviz_chart(tf.keras.utils.model_to_dot(model, show_shapes=True, expand_nested=True))
+
+except Exception as e:
+    st.error(f"An error occurred: {e}")
